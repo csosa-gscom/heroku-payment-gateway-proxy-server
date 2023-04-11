2129caf8be4567c1dde8902d2c1383677c129aae
csosa <csosa@gs-com.bz>
2023-04-11 17:27:04 -0600

    cors enabled and appi call fix

--- index.js
+++ index.js
@@ -1,53 +1,3 @@
-const express = require("express"); // http framework
-const cors = require('cors'); //CORS Policy
-const axios = require('axios'); //HTTP client for JavaScript.
-const xml2js = require('xml2js'); //xml parser
-const fs = require('fs'); //read write xml data
-const bodyParser = require('body-parser'); //
-
-
-const app = express();
-const parser = new xml2js.Parser();
-
-let port = process.env.PORT || 3000;
-
-
-
-app.get("/", (req, res)=>{
-    res.send("GS-COM Payment Gateway Proxy Server");
-});
-
-// middleware to parse XML requests
-app.use(bodyParser.raw({type: 'text/xml'}));
-
-app.post('/send-xml', (req, res) => {
-    const data = req.body;
-    
-    axios.post('https://merchantapi.digiwallet.bz/Telepin', data, { headers: { 'Content-Type': 'text/xml' }})
-      .then(response => {
-        xml2js.parseString(response.data, (err, result) => {
-          if (err) {
-            res.status(500).send('An error occurred while parsing the response.');
-          } else {
-            res.set('Content-Type', 'text/xml');
-            res.send(response.data);
-          }
-        });
-      })
-      .catch(error => {
-        console.log(error);
-        res.status(500).send('An error occurred while making the request.');
-      });
-  });
-
-
-app.listen(port, ()=>{
-    console.log(`http://localhost:${port}`);
-});
-
-
-//CORS ENABLED
-
 // const express = require("express"); // http framework
 // const cors = require('cors'); //CORS Policy
 // const axios = require('axios'); //HTTP client for JavaScript.
@@ -61,7 +11,7 @@ app.listen(port, ()=>{
 
 // let port = process.env.PORT || 3000;
 
-// app.use(cors());
+
 
 // app.get("/", (req, res)=>{
 //     res.send("GS-COM Payment Gateway Proxy Server");
@@ -94,6 +44,56 @@ app.listen(port, ()=>{
 // app.listen(port, ()=>{
 //     console.log(`http://localhost:${port}`);
 // });
+
+
+//CORS ENABLED
+
+const express = require("express"); // http framework
+const cors = require('cors'); //CORS Policy
+const axios = require('axios'); //HTTP client for JavaScript.
+const xml2js = require('xml2js'); //xml parser
+const fs = require('fs'); //read write xml data
+const bodyParser = require('body-parser'); //
+
+
+const app = express();
+const parser = new xml2js.Parser();
+
+let port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.get("/", (req, res)=>{
+    res.send("GS-COM Payment Gateway Proxy Server");
+});
+
+// middleware to parse XML requests
+app.use(bodyParser.raw({type: 'text/xml'}));
+
+app.post('/send-xml', (req, res) => {
+    const data = req.body;
+    
+    axios.post('https://merchantapi.digiwallet.bz/Telepin', data, { headers: { 'Content-Type': 'text/xml' }})
+      .then(response => {
+        xml2js.parseString(response.data, (err, result) => {
+          if (err) {
+            res.status(500).send('An error occurred while parsing the response.');
+          } else {
+            res.set('Content-Type', 'text/xml');
+            res.send(response.data);
+          }
+        });
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).send('An error occurred while making the request.');
+      });
+  });
+
+
+app.listen(port, ()=>{
+    console.log(`http://localhost:${port}`);
+});
 
 //STRICT CORS ENABLED
 // const express = require("express");
