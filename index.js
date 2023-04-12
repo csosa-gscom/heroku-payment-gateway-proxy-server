aeb987e3f138fe7ebf9536c8546cd93e2e7bbba7
csosa <csosa@gs-com.bz>
2023-04-12 10:36:24 -0600

    SalesMerchant and ExectoSelf

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-// NO CORS
 // const express = require("express"); // http framework
 // const cors = require('cors'); //CORS Policy
 // const axios = require('axios'); //HTTP client for JavaScript.
 // const xml2js = require('xml2js'); //xml parser
-// const fs = require('fs'); //read write xml data
 // const bodyParser = require('body-parser'); //
 
 
@@ -12,151 +10,68 @@
 
 // let port = process.env.PORT || 3000;
 
-// app.get("/", (req, res)=>{
-//     res.send("GS-COM Payment Gateway Proxy Server");
-// });
+// // const corsOptions = {
+// //   origin: ['*'],
+// //   methods: ['GET', 'POST']
+// // };
 
-// // middleware to parse XML requests
-// app.use(bodyParser.raw({type: 'text/xml'}));
-
-// app.post('/send-xml', (req, res) => {
-//     const data = req.body;
-
-//     axios.post('https://merchantapi.digiwallet.bz/Telepin', data, { headers: { 'Content-Type': 'text/xml' }})
-//       .then(response => {
-//         xml2js.parseString(response.data, (err, result) => {
-//           if (err) {
-//             res.status(500).send('An error occurred while parsing the response.');
-//           } else {
-//             res.set('Content-Type', 'text/xml');
-//             res.send(response.data);
-//           }
-//         });
-//       })
-//       .catch(error => {
-//         console.log(error);
-//         res.status(500).send('An error occurred while making the request.');
-//       });
-//   });
-
-
-// app.listen(port, ()=>{
-//     console.log(`http://localhost:${port}`);
-// });
-
-
-//CORS ENABLED - WORKING
-
-// const express = require("express"); // http framework
-// const cors = require('cors'); //CORS Policy
-// const axios = require('axios'); //HTTP client for JavaScript.
-// const xml2js = require('xml2js'); //xml parser
-// const fs = require('fs'); //read write xml data
-// const bodyParser = require('body-parser'); //
-
-
-// const app = express();
-// const parser = new xml2js.Parser();
-
-// let port = process.env.PORT || 3000;
+// // app.use(cors(corsOptions));
 
 // app.use(cors());
-
-// app.get("/", (req, res)=>{
-//     res.send("GS-COM Payment Gateway Proxy Server");
-// });
-
-// // middleware to parse XML requests
-// app.use(bodyParser.raw({type: 'text/xml'}));
-
-// app.post('/send-xml', (req, res) => {
-//     const data = req.body;
-
-//     axios.post('https://merchantapi.digiwallet.bz/Telepin', data, { headers: { 'Content-Type': 'text/xml' }})
-//       .then(response => {
-//         xml2js.parseString(response.data, (err, result) => {
-//           if (err) {
-//             res.status(500).send('An error occurred while parsing the response.');
-//           } else {
-//             res.set('Content-Type', 'text/xml');
-//             res.send(response.data);
-//           }
-//         });
-//       })
-//       .catch(error => {
-//         console.log(error);
-//         res.status(500).send('An error occurred while making the request.');
-//       });
-//   });
-
-
-// app.listen(port, ()=>{
-//     console.log(`http://localhost:${port}`);
-// });
-
-// STRICT CORS ENABLED - not working
-// const express = require("express");
-// const cors = require("cors");
-// const axios = require("axios");
-// const xml2js = require("xml2js");
-// const fs = require("fs");
-// const bodyParser = require("body-parser");
-
-// const app = express();
-// const parser = new xml2js.Parser();
-// const port = process.env.PORT || 3000;
-
-// const corsOptions = {
-//   origin: ['*'],
-//   methods: ['GET', 'POST']
-// };
-
-// app.use(cors(corsOptions));
-
-
 
 // app.get("/", (req, res) => {
 //   res.send("GS-COM Payment Gateway Proxy Server");
 // });
 
-// app.use(bodyParser.raw({ type: "text/xml" }));
+// // middleware to parse XML requests
+// app.use(bodyParser.raw({ type: 'text/xml' }));
 
-// app.post("/send-xml", cors(corsOptions), (req, res) => {
+// app.post('/send-xml', (req, res) => {
 //   const data = req.body;
 
-//   axios
-//     .post("https://merchantapi.digiwallet.bz/Telepin", data, {
-//       headers: { "Content-Type": "text/xml",
-//       'Access-Control-Allow-Origin': '*'},
-//     })
-//     .then((response) => {
-//       xml2js.parseString(response.data, (err, result) => {
-//         if (err) {
-//           res.status(500).send("An error occurred while parsing the response.");
-//         } else {
-//           res.set("Content-Type", "text/xml");
-//           res.send(response.data);
-//         }
-//       });
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//       res.status(500).send("An error occurred while making the request.");
-//     });
+//   // Parse XML data into a JavaScript object
+//   xml2js.parseString(data, (err, result) => {
+//     if (err) {
+//       res.status(500).send('An error occurred while parsing the request.');
+//     } else {
+//       // Modify the object properties as desired
+//       result.TCSRequest.UserName = '501203278252';
+//       result.TCSRequest.Password = '052004';
+//       result.TCSRequest.Function[0].Param1 = '977';
+//       result.TCSRequest.Function[0].Param6 = '5012235128';
+
+//       // Convert the object back into XML
+//       const builder = new xml2js.Builder();
+//       const updatedData = builder.buildObject(result);
+
+//       // Send the updated data to the API
+//       axios.post('https://merchantapi.digiwallet.bz/Telepin', updatedData, { headers: { 'Content-Type': 'text/xml' } })
+//         .then(response => {
+//           xml2js.parseString(response.data, (err, result) => {
+//             if (err) {
+//               res.status(500).send('An error occurred while parsing the response.');
+//             } else {
+//               res.set('Content-Type', 'text/xml');
+//               res.send(response.data);
+//             }
+//           });
+//         })
+//         .catch(error => {
+//           console.log(error);
+//           res.status(500).send('An error occurred while making the request.');
+//         });
+//     }
+//   });
 // });
 
 // app.listen(port, () => {
 //   console.log(`http://localhost:${port}`);
 // });
 
-
-//Security Proxy Server
-
 const express = require("express"); // http framework
 const cors = require('cors'); //CORS Policy
 const axios = require('axios'); //HTTP client for JavaScript.
 const xml2js = require('xml2js'); //xml parser
-const fs = require('fs'); //read write xml data
 const bodyParser = require('body-parser'); //
 
 
@@ -164,6 +79,13 @@ const app = express();
 const parser = new xml2js.Parser();
 
 let port = process.env.PORT || 3000;
+
+// const corsOptions = {
+//   origin: ['*'],
+//   methods: ['GET', 'POST']
+// };
+
+// app.use(cors(corsOptions));
 
 app.use(cors());
 
@@ -183,10 +105,16 @@ app.post('/send-xml', (req, res) => {
       res.status(500).send('An error occurred while parsing the request.');
     } else {
       // Modify the object properties as desired
-      result.TCSRequest.UserName = '501203278252';
-      result.TCSRequest.Password = '052004';
-      result.TCSRequest.Function[0].Param1 = '977';
-      result.TCSRequest.Function[0].Param6 = '5012235128';
+      if (result.TCSRequest.Function[0].$.name === 'SALESREQUESTEXECTOSELF'){
+        result.TCSRequest.UserName = '501203278252';
+        result.TCSRequest.Password = '052004';
+      } else if (result.TCSRequest.Function[0].$.name === 'SALESREQUESTMERCHANT_OTP') {
+        result.TCSRequest.UserName = '501203278252';
+        result.TCSRequest.Password = '052004';
+        result.TCSRequest.Function[0].Param1 = '977';
+        result.TCSRequest.Function[0].Param6 = '5012235128';
+      }
+
 
       // Convert the object back into XML
       const builder = new xml2js.Builder();
